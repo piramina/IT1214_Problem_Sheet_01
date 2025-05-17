85a24ce1915d6b1ed342db9bba569f5433699be3
piramina <piraminasasikaran26@gmail.com>
2025-05-17 10:11:00 +0000

    Created 01.java

--- 01.java
+++ 01.java
@@ -8,4 +8,25 @@ class Student{
         this.name=name;
         this.dayAttended=dayAttended;
     }
+
+    int getStudentid(){
+        return studentid;
+    }
+    void setStudentid(int studentid){
+        this.studentid=studentid;
+    }
+
+    String getName(){
+        return name;
+    }
+    void setName(String name){
+        this.name=name;
+    }
+
+    int getDaysAttended(){
+        return dayAttended;
+    }
+    void setDaysAttended(int daysAttended){
+        this.daysAttended=daysAttened;
+    }
 }
