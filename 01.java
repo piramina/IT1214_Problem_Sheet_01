f92e0ec4cae9963a249fadfa515bd70076eab310
piramina <piraminasasikaran26@gmail.com>
2025-05-17 10:02:16 +0000

    Created 02.java

--- 01.java
+++ 01.java
@@ -1,1 +1,11 @@
+class Student{
+    private int studentid;
+    private String name;
+    private int dayAttended;
 
+    Student(){
+        this.studentid=studentid;
+        this.name=name;
+        this.dayAttended=dayAttended;
+    }
+}
